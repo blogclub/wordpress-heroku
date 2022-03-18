4109b634ed988dd341c0cbfafdad9cf2d2d3b0e6
tobeherenow <zyfa2008@gmail.com>
2022-03-18 18:52:08 +0800

    Update wp-cache-config.php

--- web/app/wp-cache-config.php
+++ web/app/wp-cache-config.php
@@ -5,13 +5,17 @@ WP-Cache Config Sample File
 See wp-cache.php for author details.
 */
 
+$wp_cache_home_path = '/';
+$wp_cache_slash_check = 1;
+$cache_page_secret = 'd490f99019ee571d284197da8ee7fab8';
+$cache_time_interval = '600';
 if ( ! defined('WPCACHEHOME') )
-	define( 'WPCACHEHOME', WP_PLUGIN_DIR . '/wp-super-cache/' );
+        define( 'WPCACHEHOME', WP_CONTENT_DIR . "/plugins/wp-super-cache/" );
 
 $cache_compression = 0; // Super cache compression
-$cache_enabled = false;
+$cache_enabled = true;
 $super_cache_enabled = true;
-$cache_max_time = 0; //in seconds
+$cache_max_time = 0;
 //$use_flock = true; // Set it true or false if you know what to use
 $cache_path = WP_CONTENT_DIR . '/cache/';
 $file_prefix = 'wp-cache-';
@@ -31,10 +35,10 @@ $cache_rebuild_files = 1;
 $wp_cache_mutex_disabled = 1;
 
 // Just modify it if you have conflicts with semaphores
-$sem_id = 5419;
+$sem_id = 1700987490;
 
 if ( '/' != substr($cache_path, -1)) {
-	$cache_path .= '/';
+        $cache_path .= '/';
 }
 
 $wp_cache_mobile = 0;
@@ -73,11 +77,11 @@ $wp_cache_pages[ "pages" ] = 0;
 $wp_cache_pages[ "single" ] = 0;
 $wp_cache_pages[ "author" ] = 0;
 $wp_cache_hide_donation = 0;
-$wp_cache_not_logged_in = 0;
+$wp_cache_not_logged_in = 2;
 $wp_cache_clear_on_post_edit = 0;
 $wp_cache_hello_world = 0;
 $wp_cache_mobile_enabled = 0;
-$wp_cache_cron_check = 0;
+$wp_cache_cron_check = 1;
 $wp_cache_mfunc_enabled = 0;
 $wp_cache_make_known_anon = 0;
 $wp_cache_refresh_single_only = 0;
@@ -95,11 +99,11 @@ $wp_cache_preload_taxonomies = 0;
 $wp_cache_preload_email_me = 0;
 $wp_cache_preload_email_volume = 'none';
 $wp_cache_mobile_prefixes = '';
-$cached_direct_pages = array();
+$cached_direct_pages = array(  );
 $wpsc_served_header = false;
 $cache_gc_email_me = 0;
 $wpsc_save_headers = 0;
-$cache_schedule_interval = 'daily';
+$cache_schedule_interval = 'hourly';
 $wp_super_cache_comments = 1;
 $wpsc_version = 169;
 ?>
